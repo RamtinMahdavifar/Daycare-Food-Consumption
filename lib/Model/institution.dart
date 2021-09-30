a561c4a701d11d489f259b10af0f1dd53f0512da
Lec337 <lec337@mail.usask.ca>
2021-09-30 00:29:52 -0600

    Implemented Institution class getters and setters.

--- lib/Model/institution.dart
+++ lib/Model/institution.dart
@@ -6,4 +6,20 @@ class Institution {
 
   // Institution constructor
   Institution(this._name, this._address);
+
+  String get name{
+    return this._name;
+  }
+
+  String get address{
+    return this._address;
+  }
+
+  set name(String newName){
+    this._name = newName;
+  }
+
+  set address(String newAddress){
+    this._address = newAddress;
+  }
 }
