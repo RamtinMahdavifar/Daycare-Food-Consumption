75fe79c2d59a11ee426fcd99e20af7b72b48e9f4
Lec337 <lec337@mail.usask.ca>
2021-09-29 22:18:40 -0600

    Added database.dart file, implemented Database class constructor using the singleton pattern.

---
+++ lib/Model/database.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+/// Class to a access the firebase database, this class is implemented using the
+/// singleton pattern and provides methods to read and write data to and from
+/// Firebase
+class Database {
+  // private instance of our firebase database
+  static final FirebaseDatabase _databaseInstance = FirebaseDatabase.instance;
+
+  // initialize the singleton instance of our Database class
+  static final Database _instance = Database._privateConstructor();
+
+  // define a private constructor for this class which will allocate memory etc
+  // to class, this is only call-able from within this class.
+  Database._privateConstructor();
+
+  // define a factory constructor for this class, this constructor can be called
+  // as Database(), this constructor is the only non-private constructor for this
+  // class, so this is the only way this class can be instantiated from outside
+  // of this class, this ensures that there is only one instance of our database
+  // at once
+  factory Database() {
+    return _instance;
+  }
+}
