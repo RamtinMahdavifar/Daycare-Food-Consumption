918c637092cd047124d86f2990b3f7851de694d6
Lec337 <lec337@mail.usask.ca>
2021-09-29 23:40:14 -0600

    Added tests for Institution class getters and setters

--- test/institution_test.dart
+++ test/institution_test.dart
@@ -1,2 +1,22 @@
 import 'package:test/test.dart';
-import 'package:plate_waste_recorder/Model/database.dart';
+import 'package:plate_waste_recorder/Model/institution.dart';
+
+// run tests from the terminal using: flutter test test/institution_test.dart
+void main(){
+  group("institution getters and setters",() {
+    test("institution getters", () {
+      final testInstitution = Institution("test institution", "test address");
+      expect(testInstitution.name, "test institution");
+      expect(testInstitution.address, "test address");
+    });
+
+    test("institution setters", () {
+      final testInstitution = Institution("test institution", "test address");
+      testInstitution.name = "new test name";
+      testInstitution.address = "new test address";
+      expect(testInstitution.name, "new test name");
+      expect(testInstitution.address, "new test address");
+    });
+  });
+
+}
