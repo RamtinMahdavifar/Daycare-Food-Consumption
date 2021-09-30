7b08d35a4b8a155dba171d2a75a75f254c2bf2ca
Lec337 <lec337@mail.usask.ca>
2021-09-30 04:00:09 -0600

    Implemented ResearcherInfo object using name as database key for now

---
+++ lib/Model/researcher_info.dart
@@ -0,0 +1,9 @@
+
+import 'package:plate_waste_recorder/Model/info.dart';
+
+class ResearcherInfo extends Info{
+  ResearcherInfo(String name){
+    this.name = name;
+    this.databaseKey = name;
+  }
+}
