9284d7361350701d4fc2aef38dd8b4d1fa695ad0
Lec337 <lec337@mail.usask.ca>
2021-09-30 01:30:55 -0600

    Added info.dart and added abstract class and fields

---
+++ lib/Model/info.dart
@@ -0,0 +1,12 @@
+
+/// Abstract Info class that specifies fields and methods for info classes.
+abstract class Info{
+  // specify attributes as late so they don't have to be provided on instantiation
+  // as we cannot instantiate an abstract class
+
+  // name of the component being represented by this info object
+  late String name;
+  // key on the database that can be used to lookup the item being represented
+  // by this info object
+  late String databaseKey;
+}
