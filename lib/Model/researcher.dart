769c3e2b798ba25a2856f4c6c0ce534d3bc359fc
Lec337 <lec337@mail.usask.ca>
2021-09-30 04:02:48 -0600

    Added getResearchInfo method

--- lib/Model/researcher.dart
+++ lib/Model/researcher.dart
@@ -1,11 +1,15 @@
 
+import 'package:plate_waste_recorder/Model/researcher_info.dart';
+
 class Researcher{
   String _researcherName;
   List<String> _researchGroupInfos = [];
 
   Researcher(this._researcherName);
 
-
+  ResearcherInfo getResearcherInfo(){
+    return ResearcherInfo(this._researcherName);
+  }
 
 
 
