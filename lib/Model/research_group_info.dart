98827cd8118f23f00724b16ba1dcb324eefd392b
Lec337 <lec337@mail.usask.ca>
2021-09-30 04:08:46 -0600

    Added ResearchGroupInfo class using group name as database key for now

---
+++ lib/Model/research_group_info.dart
@@ -0,0 +1,9 @@
+
+import 'package:plate_waste_recorder/Model/info.dart';
+
+class ResearchGroupInfo extends Info{
+  ResearchGroupInfo(String name){
+    this.name = name;
+    this.databaseKey = name;
+  }
+}
