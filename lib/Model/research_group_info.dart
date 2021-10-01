a5205540356a9c2bd3a7dda5d51e41ba0ba558a9
Lec337 <lec337@mail.usask.ca>
2021-10-01 03:21:59 -0600

    Added methods to construct objects from json at every level, a ResearchGroup object is now created in main.dart, the institutions of this group are displayed

--- lib/Model/research_group_info.dart
+++ lib/Model/research_group_info.dart
@@ -2,8 +2,17 @@
 import 'package:plate_waste_recorder/Model/info.dart';
 
 class ResearchGroupInfo extends Info{
+  // must override these fields here or else fields cannot be used in the JSON constructor
+  String name = "";
+  String databaseKey = "";
+
   ResearchGroupInfo(String name){
     this.name = name;
     this.databaseKey = name;
   }
+
+  // this is considered a constructor and so cannot be inherited from our super Info
+  ResearchGroupInfo.fromJSON(Map<String, dynamic> json)
+      : name = json["name"].toString(),
+        databaseKey = json["databaseKey"];
 }
