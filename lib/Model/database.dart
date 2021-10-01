cc54aa2c0c4527b591b6857bb139095654dd789c
Lec337 <lec337@mail.usask.ca>
2021-10-01 00:49:23 -0600

    Updated writes to database to write objects as Maps

--- lib/Model/database.dart
+++ lib/Model/database.dart
@@ -16,6 +16,9 @@ class Database {
   // initialize the singleton instance of our Database class
   static final Database _instance = Database._privateConstructor();
 
+  // initialize the location research groups are to be stored on the database
+  final String _researchGroupRootLocation = "Research Groups";
+
   // define a private constructor for this class which will allocate memory etc
   // to class, this is only call-able from within this class.
   Database._privateConstructor();
@@ -32,9 +35,16 @@ class Database {
   void writeInstitution(Institution institution, ResearchGroupInfo currentResearchGroupInfo){
     InstitutionInfo currentInstitutionInfo = institution.getInstitutionInfo();
     DatabaseReference institutionReference = _databaseInstance.reference()
+        .child(this._researchGroupRootLocation)
         .child(currentResearchGroupInfo.databaseKey)
         .child(currentInstitutionInfo.databaseKey);
-    institutionReference.set(institution);
+    // convert the input institution and all data structures and fields of this object
+    // to JSON
+    String institutionJSON = jsonEncode(institution);
+
+    // convert the produced JSON to a map which can be stored on our database
+    Map<String, dynamic> institutionMap = json.decode(institutionJSON);
+    institutionReference.set(institutionMap);
   }
 
   void readInstitution(InstitutionInfo institutionInfo, ResearchGroupInfo currentResearchGroupInfo,
@@ -72,7 +82,14 @@ class Database {
   void writeResearchGroup(ResearchGroup researchGroup){
     ResearchGroupInfo researchGroupInfo = researchGroup.getResearchGroupInfo();
     DatabaseReference researchGroupDatabaseReference = _databaseInstance.reference()
-      .child(researchGroupInfo.databaseKey);
-    researchGroupDatabaseReference.set(researchGroup);
+        .child(this._researchGroupRootLocation)
+        .child(researchGroupInfo.databaseKey);
+    // convert the ResearchGroup object to JSON before writing to the db, this also
+    // converts fields and data structures within this object to JSON
+    String researchGroupJSON = jsonEncode(researchGroup);
+    // we cannot write raw JSON to the database, decode this JSON to get a map which
+    // preserves the structure of our object when written to the database
+    Map<String, dynamic> researchGroupAsMap = json.decode(researchGroupJSON);
+    researchGroupDatabaseReference.set(researchGroupAsMap);
   }
 }
