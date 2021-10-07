699a77d7bc4ef95023f91cf4146b6f46f5f61500
jes857 <jes857@usask.ca>
2021-10-07 05:54:09 -0600

    removed upload buttons after image is selected and replaced with area to put comments

--- lib/View/upload_data.dart
+++ lib/View/upload_data.dart
@@ -1,92 +1,228 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'widgets.dart' as btn;
+import 'package:flutter/foundation.dart';
 import 'dart:io';
+import 'dart:async';
+import 'package:image_picker/image_picker.dart';
 
 class UploadData extends StatefulWidget {
   //String institutionName;
   //String institutionAddress;
 
-  UploadData({Key? key}) : super(key: key);
+  const UploadData({Key? key}) : super(key: key);
 
   @override
   State<UploadData> createState() => _UploadDataState();
 }
 
-class _UploadDataState extends State<UploadData> {
+
+class _UploadDataState extends State<UploadData>{
+
+  List<XFile>? _imageFileList;
+
+  set _imageFile(XFile? value) {
+    _imageFileList = value == null ? null : [value];
+  }
+
+  dynamic _pickImageError;
+  //bool isVideo = false;
+
+
+  String? _retrieveDataError;
+  bool _showButton = true;
+  final ImagePicker _picker = ImagePicker();
+  final TextEditingController maxWidthController = TextEditingController();
+  final TextEditingController maxHeightController = TextEditingController();
+  final TextEditingController qualityController = TextEditingController();
+
+
+  void _onImageButtonPressed(ImageSource source,
+      {BuildContext? context, bool isMultiImage = false}) async {
+
+    await _displayPickImageDialog(context!,
+            (double? maxWidth, double? maxHeight, int? quality) async {
+          try {
+            final pickedFile = await _picker.pickImage(
+              source: source,
+              maxWidth: maxWidth,
+              maxHeight: maxHeight,
+              imageQuality: quality,
+            );
+            setState(() {
+              _imageFile = pickedFile;
+              hideButton();
+            });
+          } catch (e) {
+            setState(() {
+              _pickImageError = e;
+            });
+          }
+        });
+    }
+
+  Widget addComments() {
+    return TextField(
+      decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          hintText: 'Comments'
+      ),
+    );
+  }
+
+  Widget _previewImages() {
+    final Text? retrieveError = _getRetrieveErrorWidget();
+    if (retrieveError != null) {
+      return retrieveError;
+    }
+    if (_imageFileList != null) {
+      return Semantics(
+          child: ListView.builder(
+            key: UniqueKey(),
+            itemBuilder: (context, index) {
+              // Why network for web?
+              // See https://pub.dev/packages/image_picker#getting-ready-for-the-web-platform
+              return Semantics(
+                label: 'image_picker_example_picked_image',
+                child: kIsWeb
+                    ? Image.network(_imageFileList![index].path)
+                    : Image.file(File(_imageFileList![index].path)),
+              );
+            },
+            itemCount: _imageFileList!.length,
+          ),
+          label: 'image_picker_example_picked_images');
+    } else if (_pickImageError != null) {
+      return Text(
+        'Pick image error: $_pickImageError',
+        textAlign: TextAlign.center,
+      );
+    } else {
+      return const Text(
+        'You have not yet picked an image.',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
+  Widget _handlePreview() {
+    return _previewImages();
+  }
+
+  void hideButton(){
+    setState((){
+      _showButton = !_showButton;
+    });
+  }
+
+  Future<void> retrieveLostData() async {
+    final LostDataResponse response = await _picker.retrieveLostData();
+    if (response.isEmpty) {
+      return;
+    }
+    if (response.file != null) {
+
+      setState(() {
+        _imageFile = response.file;
+        _imageFileList = response.files;
+      });
+    } else {
+      _retrieveDataError = response.exception!.code;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text("Upload Data"), leading: btn.BackButton(context)),
-        body: Container(
-            margin: EdgeInsets.symmetric(horizontal: 8.0),
-            child: Column(
-                children: [ //
-                  btn.uploadImage(context),
-                ])
-        )
-    );
-  }
-}
-
-class ImageCapture extends StatefulWidget{
-  createState() => _ImageCaptureState();
-}
-
-class _ImageCaptureState extends State<ImageCapture>{
-  late File _imageFile;
-
-  Future<void> _pickImage(ImageSource source) async{
-    File selected = await ImagePicker.pickImage(source: source);
-
-    setState(() {
-      _imageFile = selected;
-    });
-
-  }
-
-  void _clear() {
-    setState(() => _imageFile = null);
-  }
-
-
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.photo_camera),
-              onPressed: () => _pickImage(ImageSource.camera)
-            ),
-            IconButton(
-                icon: Icon(Icons.photo_library),
-                onPressed: () => _pickImage(ImageSource.gallery)
-            )
-
-          ],
-        ),
+      appBar: AppBar(
+        title: Text("Upload Data"),
       ),
-      body: ListView(
+      body: Center(
+        child: !kIsWeb && defaultTargetPlatform == TargetPlatform.android
+            ? FutureBuilder<void>(
+          future: retrieveLostData(),
+          builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return const Text(
+                  'You have not yet picked an image.',
+                  textAlign: TextAlign.center,
+                );
+              case ConnectionState.done:
+                return _handlePreview();
+              default:
+                if (snapshot.hasError) {
+                  return Text(
+                    'Pick image/video error: ${snapshot.error}}',
+                    textAlign: TextAlign.center,
+                  );
+                } else {
+                  return const Text(
+                    'You have not yet picked an image.',
+                    textAlign: TextAlign.center,
+                  );
+                }
+            }
+          },
+        )
+            : _handlePreview(),
+      ),
+
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
-          if _imageFile != null ... [
-            Image.file(_imageFile),
-            Row(
-              children: <Widget>[
-                TextButton(
-                  child: Icon(Icons.refresh),
-                  onPressed: _clear,
-                )
-              ]
-
-            )
-          ]
-        ]
-      )
-
+          Visibility(
+            //label: 'image_picker_example_from_gallery',
+            visible: _showButton,
+            child: FloatingActionButton(
+              onPressed: () {
+                _onImageButtonPressed(ImageSource.gallery, context: context);
+              },
+              heroTag: 'image0',
+              tooltip: 'Pick Image from gallery',
+              child: const Icon(Icons.photo),
+            ),
+          ),
+          Visibility(
+            visible: _showButton,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: FloatingActionButton(
+                onPressed: () {
+                  _onImageButtonPressed(ImageSource.camera, context: context);
+                },
+                heroTag: 'image2',
+                tooltip: 'Take a Photo',
+                child: const Icon(Icons.camera_alt),
+              ),
+            ),
+          ),
+          Visibility(
+            visible: !_showButton,
+            child: addComments()
+          )
+        ],
+      ),
     );
   }
+
+  Text? _getRetrieveErrorWidget() {
+    if (_retrieveDataError != null) {
+      final Text result = Text(_retrieveDataError!);
+      _retrieveDataError = null;
+      return result;
+    }
+    return null;
+  }
+
+  Future<void> _displayPickImageDialog(
+      BuildContext context, OnPickImageCallback onPick) async {
+    onPick(null, null, null);//(width, height, quality);
+
+  }
 }
+
+
+typedef void OnPickImageCallback(
+    double? maxWidth, double? maxHeight, int? quality);
 
