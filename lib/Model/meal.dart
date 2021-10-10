b1ece25c09475aefdc9445805b94737fc82bda82
Shantanu Mishra <shantanu.mishra@usask.ca>
2021-10-10 13:59:52 -0600

    Added meal class

---
+++ lib/Model/meal.dart
@@ -0,0 +1,41 @@
+
+
+
+
+enum MealType{
+  before,
+  after
+}
+
+/// Class representing a Meal, which the food east by a subject
+/// a subject can have multiple meals in a day
+class Meal{
+
+  int _mealID;
+  String _imagePath;
+  MealType _mealType;
+  late String _comment;
+
+  Meal(this._mealID,this._imagePath,this._mealType);
+
+  int get id{
+    return this._mealID;
+  }
+
+  String get image{
+    return this._imagePath;
+  }
+
+  String get comment{
+    return this._comment;
+  }
+
+  MealType get mealType{
+    return this._mealType;
+  }
+
+  set comment(String newComment){
+    this._comment = newComment;
+  }
+
+}
