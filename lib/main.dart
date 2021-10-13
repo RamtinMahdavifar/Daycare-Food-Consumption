21f3a1124ac569b92f5b82ef2f06ffb8234aee74
Lec337 <lec337@mail.usask.ca>
2021-10-12 22:23:13 -0600

    Removed deprecated comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:plate_waste_recorder/Model/database.dart';
-//import 'Model/institution.dart';
-//import 'institution_page.dart';
 import 'package:plate_waste_recorder/View/select_institution.dart';
-//import 'View/select_institution.dart';
-import 'package:plate_waste_recorder/Model/research_group_info.dart';
-import 'package:firebase_database/firebase_database.dart'; // need to include for the Event data type
-import 'package:plate_waste_recorder/Model/research_group.dart';
-import 'package:plate_waste_recorder/Model/researcher_info.dart';
-import 'package:plate_waste_recorder/Model/institution_info.dart';
-import 'dart:convert'; // required for jsonDecode()
+
 
 void main() {
   // TODO: remove database initialization
