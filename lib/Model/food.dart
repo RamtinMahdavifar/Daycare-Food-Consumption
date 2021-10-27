d5e10d984941ba4e28a2e6ef61aa797d7d4ffc5c
jes857 <jes857@usask.ca>
2021-10-26 19:56:15 -0600

    removed unworking code for merging to dev

--- lib/Model/food.dart
+++ lib/Model/food.dart
@@ -17,49 +17,30 @@ class BackendService {
   }
 }
 
-class FoodBank{
+class FoodBank {
 
-  static final List<String> foods = [];
-  static void getFoodNames() async{
+ static final List<String> foods = [
+    'Apple',
+    'Banana',
+    'Fruit Gushers',
+    'Steak',
+    'Mac and Cheese',
+    'Hot Dog',
+    'Fruit roll up',
+    'Oreos',
+    'Orange Juice',
+    'Apple Juice',
+    'Dog Food',
+    'my sanity',
+    'peanut butter sandwich',
+  ];
 
-    File f = File("/food_names.csv");
-
-
-    final input = f.openRead();
-    final fields = await input.transform(utf8.decoder).transform(new CsvToListConverter()).toList();
-    foods.add(fields.toString());
-    print("Fields added");
-    print(fields);
-
-  }
-
-
-
-
-
-  //final foods = await input.transform(utf8.decoder).transform(new CsvToListConverter()).toList();
-  //assert(foods.toString() == [[',b', 3.1, 42], ['n\n']].toString());
- /* static final List<String> foods = [
-    'Beirut',
-    'Damascus',
-    'San Fransisco',
-    'Rome',
-    'Los Angeles',
-    'Madrid',
-    'Bali',
-    'Barcelona',
-    'Paris',
-    'Bucharest',
-    'New York City',
-    'Philadelphia',
-    'Sydney',
-  ];*/
 
   static List<String> getSuggestions(String query) {
     List<String> matches = <String>[];
     matches.addAll(foods);
-    print("foods");
-    print(foods);
+    //print("foods");
+    //print(foods);
 
     matches.retainWhere((s) => s.toLowerCase().contains(query.toLowerCase()));
     return matches;
