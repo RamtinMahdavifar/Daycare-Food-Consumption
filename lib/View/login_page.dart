e534af2ad6dcd91b4cd6391fa9fed3fa69702835
Lec337 <lec337@mail.usask.ca>
2021-10-27 22:29:45 -0600

    Defined widgets for the email and password input fields, invoked these from the login page

--- lib/View/login_page.dart
+++ lib/View/login_page.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:plate_waste_recorder/View/login_page_widgets.dart';
 
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: 
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            emailField(new TextEditingController()),
+            passwordField(new TextEditingController())
+          ]
+        )
+      )
     );
   }
 }
+
