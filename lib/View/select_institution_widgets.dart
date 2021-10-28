c9311dc937d08c2deb183c674877595e57823969
kepalr <67074178+kepalr@users.noreply.github.com>
2021-10-28 17:54:13 -0600

    camera API works in that it shows the viewfinder, further implementation required -> capturing image

--- lib/View/select_institution_widgets.dart
+++ lib/View/select_institution_widgets.dart
@@ -4,7 +4,7 @@ import 'package:plate_waste_recorder/View/upload_data.dart';
 import '../Model/institution.dart';
 import 'add_institutions_form.dart';
 import 'institution_page.dart';
-import 'name_suggest.dart';
+import 'camera_food.dart';
 import 'qrcode.dart';
 import 'package:plate_waste_recorder/Model/research_group_info.dart';
 import 'package:firebase_database/firebase_database.dart'; // need to include for the Event data type
@@ -157,7 +157,7 @@ Widget quickfixButton(BuildContext context){
         // pass the name of the clicked on institution to the daycare screen
         Navigator.push(context, MaterialPageRoute(
             builder: (context){
-              return ChooseFood();
+              return CameraFood();
             }));
       },
       child: Icon(Icons.edit)
