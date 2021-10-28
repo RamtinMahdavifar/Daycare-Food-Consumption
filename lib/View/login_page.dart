961bda86374984f7798557c8895f00d6ed82ec39
Lec337 <lec337@mail.usask.ca>
2021-10-28 00:41:28 -0600

    Moved widgets from login_page_widgets to login_page to be able to access and modify state, removed login_page_widgets.dart

--- lib/View/login_page.dart
+++ lib/View/login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:plate_waste_recorder/View/login_page_widgets.dart';
-
+import 'package:plate_waste_recorder/Helper/config.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -8,6 +7,92 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailFieldController = TextEditingController();
+  final TextEditingController _passwordFieldController = TextEditingController();
+  // assume our fields are valid by default
+  final bool _emailFieldValid = true;
+  final bool _passwordFieldValid = true;
+
+  Widget emailField(TextEditingController emailFieldController){
+    return const Padding(
+        padding: EdgeInsets.all(10.0),
+        child: TextField(
+          // provide the user with a keyboard specifically for email addresses
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+              hintText: 'Email',
+              border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 5.0)
+              )
+          ),
+        )
+    );
+  }
+
+  Widget passwordField(TextEditingController passwordFieldController){
+    return const Padding(
+      padding: EdgeInsets.all(10.0),
+      child: TextField(
+        // provide the user with a keyboard specifically for inputting passwords
+          keyboardType: TextInputType.visiblePassword,
+          // hide the characters the user types
+          obscureText: true,
+          decoration: InputDecoration(
+              hintText: 'Password',
+              border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 5.0)
+              )
+          )
+      ),
+    );
+  }
+
+  Widget loginButton(TextEditingController emailFieldController, TextEditingController passwordFieldController){
+    return Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: SizedBox(
+            height: 50,
+            child:
+            ElevatedButton(
+              onPressed: (){
+                Config.log.i("pressed login button");
+
+              },
+              child: const Text("Login"),
+            )
+        )
+    );
+  }
+
+  Widget forgotPasswordButton(){
+    // display a button which is only clickable text, no border etc
+    return TextButton(
+      onPressed: (){
+        Config.log.i("pressed forgot password button");
+      },
+      // specify a slightly smaller font size than regular for this button
+      // as we want this button to be more out of the way
+      child: const Text("Forgot Password?", style: TextStyle(fontSize: 20.0)),
+    );
+  }
+
+  Widget signUpButton(){
+    // display a button which is only clickable text, no border etc
+    return TextButton(
+      onPressed: (){
+        Config.log.i("pressed sign up button");
+      },
+      // specify a slightly smaller font size than regular for this button
+      // as we want this button to be more out of the way
+      child: const Text("Sign Up", style: TextStyle(fontSize: 20.0)),
+    );
+  }
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
