e2d23831111afbf876f8183b9b6b4f41adbb7b39
Lec337 <lec337@mail.usask.ca>
2021-10-27 23:44:19 -0600

    Added buttons to login, sign up and to reset forgotten password, added these buttons to login page, specified font size for buttons in theme

--- lib/View/login_page_widgets.dart
+++ lib/View/login_page_widgets.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plate_waste_recorder/Helper/config.dart';
 
 Widget emailField(TextEditingController emailFieldController){
   return const Padding(
@@ -31,6 +32,46 @@ Widget passwordField(TextEditingController passwordFieldController){
         )
       )
     ),
+  );
+}
+
+Widget loginButton(){
+  return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: SizedBox(
+        height: 50,
+        child:
+          ElevatedButton(
+            onPressed: (){
+            Config.log.i("pressed login button");
+            },
+            child: const Text("Login"),
+        )
+      )
+    );
+}
+
+Widget forgotPasswordButton(){
+  // display a button which is only clickable text, no border etc
+  return TextButton(
+    onPressed: (){
+      Config.log.i("pressed forgot password button");
+    },
+    // specify a slightly smaller font size than regular for this button
+    // as we want this button to be more out of the way
+    child: const Text("Forgot Password?", style: TextStyle(fontSize: 20.0)),
+  );
+}
+
+Widget signUpButton(){
+  // display a button which is only clickable text, no border etc
+  return TextButton(
+    onPressed: (){
+      Config.log.i("pressed sign up button");
+    },
+    // specify a slightly smaller font size than regular for this button
+    // as we want this button to be more out of the way
+    child: const Text("Sign Up", style: TextStyle(fontSize: 20.0)),
   );
 }
 
