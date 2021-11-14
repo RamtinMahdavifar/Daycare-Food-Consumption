16411ddb4184e2a3c70c6b2b4edb3baa7ed6698a
Lec337 <lec337@mail.usask.ca>
2021-11-14 07:56:23 -0600

    Added loading animation while waiting for google authentication

--- lib/View/login_page.dart
+++ lib/View/login_page.dart
@@ -10,6 +10,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  // keep track of whether the user has clicked and is waiting for a google account
+  // popup to sign in, if so display a loading graphic
+  bool _authenticationLoading = false;
+
   Widget appIcon(){
     // return an image we get from a local directory, add padding to the top
     // so the image isn't right against the top of the screen, specify padding
@@ -20,64 +24,88 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget loginButton(){
-    return Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: SizedBox(
-            height: 70,
-            child: ElevatedButton(
-              // this particular button will be a "Sign in with Google" button
-              // as such additional restrictions apply, see https://developers.google.com/identity/branding-guidelines?hl=en
-              // for details, buttons must be pure white ie #FFFFFF
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.white),
-                // specify rounded edges for the button, add a blue border to make the button
-                // more distinct from the background
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))),
-                side: MaterialStateProperty.all(BorderSide(width: 1, color: Colors.blue))
-              ),
-              onPressed: () async {
-                Config.log.i("user has pressed login button, attempting to login via google");
-                // attempt to login to the app via google
-                try{
-                  await Authentication().googleSignIn();
+    if(this._authenticationLoading){
+      // authentication is currently loading, display a loading animation
+      Config.log.i("waiting for google authentication to complete");
+      return Center(child: CircularProgressIndicator(
+        value: null,
+        color: Colors.green,
+      ));
+    }
+    else{
+      return Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SizedBox(
+              height: 70,
+              child: ElevatedButton(
+                // this particular button will be a "Sign in with Google" button
+                // as such additional restrictions apply, see https://developers.google.com/identity/branding-guidelines?hl=en
+                // for details, buttons must be pure white ie #FFFFFF
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                      // specify rounded edges for the button, add a blue border to make the button
+                      // more distinct from the background
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))),
+                      side: MaterialStateProperty.all(BorderSide(width: 1, color: Colors.blue))
+                  ),
+                  onPressed: () async {
+                    Config.log.i("user has pressed login button, attempting to login via google");
+                    // the user has pressed the login button, change the state to reflect that
+                    // the app is waiting for the google authentication popup
+                    setState((){
+                      this._authenticationLoading = true;
+                    });
+                    // attempt to login to the app via google
+                    try{
+                      await Authentication().googleSignIn();
 
-                  // we've logged in without errors, go to the ChooseInstitute page
-                  await Navigator.push(context, MaterialPageRoute(
-                      builder: (context){
-                        return ChooseInstitute();
-                      }));
-                }
-                on FirebaseAuthException catch(e){
-                  // login error has occurred, the user has specified invalid credentials
-                  // or some error has occurred
-                  if(e.code == "account-exists-with-different-credential" || e.code == "invalid-credential"){
-                    // the user specified invalid credentials, inform the user of this via snackbar
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:
-                      const Text("Login failed due to incorrect email address or password")
-                    ));
-                  }
-                  else{
-                    // authentication failed for some other reason
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:
-                      const Text("Login failed, please try again")
-                    ));
-                  }
-                }
-              },
-              child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    // specify padding for each element of this button as we need our "Sign in with Google"
-                    // text to be to the right of the google logo, specify a grey font colour for the text or
-                    // else it doesn't show up against the white background of the button
-                    Padding(padding: EdgeInsets.only(left:0), child: Image.asset("Icons/google-logo-button.png")),
-                    Padding(padding: EdgeInsets.only(left: 10), child: Text("Sign in with Google", style: TextStyle(color: Colors.black54, fontSize: 28.0)))
-                  ]
+                      // we have finished authenticating
+                      setState((){
+                        this._authenticationLoading = false;
+                      });
+
+                      // we've logged in without errors, go to the ChooseInstitute page
+                      await Navigator.push(context, MaterialPageRoute(
+                          builder: (context){
+                            return ChooseInstitute();
+                          }));
+                    }
+                    on FirebaseAuthException catch(e){
+                      // login error has occurred, the user has specified invalid credentials
+                      // or some error has occurred
+                      // we have also finished loading our authentication
+                      setState((){
+                        this._authenticationLoading = false;
+                      });
+                      if(e.code == "account-exists-with-different-credential" || e.code == "invalid-credential"){
+                        // the user specified invalid credentials, inform the user of this via snackbar
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:
+                        const Text("Login failed due to incorrect email address or password")
+                        ));
+                      }
+                      else{
+                        // authentication failed for some other reason
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:
+                        const Text("Login failed, please try again")
+                        ));
+                      }
+                    }
+                  },
+                  child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        // specify padding for each element of this button as we need our "Sign in with Google"
+                        // text to be to the right of the google logo, specify a grey font colour for the text or
+                        // else it doesn't show up against the white background of the button
+                        Padding(padding: EdgeInsets.only(left:0), child: Image.asset("Icons/google-logo-button.png")),
+                        Padding(padding: EdgeInsets.only(left: 10), child: Text("Sign in with Google", style: TextStyle(color: Colors.black54, fontSize: 28.0)))
+                      ]
+                  )
               )
-            )
-        )
-    );
+          )
+      );
+    }
   }
 
   @override
