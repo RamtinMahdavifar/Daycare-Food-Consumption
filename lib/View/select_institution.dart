db5b53035f12225fa7125debf67828e228c1123a
Shantanu Mishra <shantanu.mishra@usask.ca>
2021-11-16 18:31:32 -0600

    removed the unused imports

--- lib/View/select_institution.dart
+++ lib/View/select_institution.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'select_institution_widgets.dart';
-import 'package:dio/dio.dart';
-import 'package:firebase_database/firebase_database.dart'; // need to include for the Event data type
-import 'package:plate_waste_recorder/Model/research_group.dart';
-import 'dart:convert'; // required for jsonDecode()
-import 'package:plate_waste_recorder/Helper/config.dart';
-import 'package:plate_waste_recorder/View/login_page.dart';
+
 
 class ChooseInstitute extends StatefulWidget {
   @override
@@ -20,7 +15,7 @@ class _ChooseInstituteState extends State<ChooseInstitute> {
     return Scaffold(
         appBar: AppBar(title: Text('Plate Waste Tracker')),
         body: Container(
-            // margin: EdgeInsets.symmetric(horizontal: 8.0),
+            margin: EdgeInsets.symmetric(horizontal: 8.0),
             child: Column(
                 children: <Widget>[
                   // TODO: move these child widgets to their own files
