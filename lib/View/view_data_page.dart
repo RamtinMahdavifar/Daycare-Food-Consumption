4191bde5953e83e30dad55ae3ef646226d1775b3
Lec337 <lec337@mail.usask.ca>
2021-11-16 23:04:44 -0600

    Defined initial stateful widget and constructor

---
+++ lib/View/view_data_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ViewDataPage extends StatefulWidget {
+  String institutionName;
+  String institutionAddress;
+
+  ViewDataPage(this.institutionName,this.institutionAddress, {Key? key}) : super(key: key);
+
+
+  @override
+  _ViewDataPageState createState() => _ViewDataPageState();
+}
+
+class _ViewDataPageState extends State<ViewDataPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
