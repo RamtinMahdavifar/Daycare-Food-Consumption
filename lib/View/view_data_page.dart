0106cabda39523ce7d3df83730d4ec9dbe78f16e
Lec337 <lec337@mail.usask.ca>
2021-11-17 18:27:33 -0600

    Updated view data page to add whitespace between button elements, added updated google-services config file

--- lib/View/view_data_page.dart
+++ lib/View/view_data_page.dart
@@ -14,10 +14,39 @@ class ViewDataPage extends StatefulWidget {
 class _ViewDataPageState extends State<ViewDataPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-            children:
-        )
+    return Scaffold(
+        appBar: AppBar(title: Text('View Data For Institution: ${widget.institutionName}')),
+      body: Center(
+          child: Column(
+              children: <Widget>[
+                SizedBox(height: 80.0),
+                Card(
+                  // TODO: make sizes relative to screen size
+                    child: SizedBox(width: 800.0, height: 80.0,
+                        child: Container(margin: EdgeInsets.only(top: 20.0, bottom: 20.0, left: 40.0, right: 40.0),
+                            child: ListTile(title: Center(child: Text("test 1"))))
+                    )
+                ),
+                SizedBox(height: 80.0), // add an empty SizedBox between column elements
+                // to create space between elements
+                Card(
+                    child: SizedBox(width: 800.0, height: 80.0,
+                        child: Container(margin: EdgeInsets.only(top: 20.0, bottom: 20.0, left: 40.0, right: 40.0),
+                            child: ListTile(
+                                title: Center(child: Text("test 2"))),
+                        )
+                    )
+                ),
+                SizedBox(height: 80.0),
+                Card(
+                    child: SizedBox(width: 800.0, height: 80.0,
+                        child: Container(margin: EdgeInsets.only(top: 20.0, bottom: 20.0, left: 40.0, right: 40.0),
+                            child: ListTile(title: Center(child: Text("test 3"))))
+                    )
+                )
+              ]
+          )
+      )
     );
   }
 }
