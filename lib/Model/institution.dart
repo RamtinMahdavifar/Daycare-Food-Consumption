123e4a29dac932fa2bbbaa75277c070ffb8c57c8
Lec337 <lec337@mail.usask.ca>
2021-11-19 17:34:52 -0600

    Fixed institutions map JSON conversion

--- lib/Model/institution.dart
+++ lib/Model/institution.dart
@@ -145,7 +145,7 @@ class Institution {
   Map<String, dynamic> toJson() => {
     '_name': this._name,
     '_address': this._address,
-    '_subjectsMap': jsonEncode(this._subjectsMap)
+    '_subjectsMap': this._subjectsMap
   };
 
   Institution.fromJSON(Map<String, dynamic> json)
