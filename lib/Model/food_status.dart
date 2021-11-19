a3170be02627f39fd0945054a0c3787468f72a90
Lec337 <lec337@mail.usask.ca>
2021-11-19 03:58:40 -0600

    Added view option in food status enum

--- lib/Model/food_status.dart
+++ lib/Model/food_status.dart
@@ -4,8 +4,11 @@
 /// leftover food not finished
 /// container: for underlying containers used to house the meal or other artifacts like wrappers, inedible
 /// portions of food like peels
+/// view: this particular status is used when we don't want to record any data for a meal
+/// but instead want to view that meal and see it's data
 enum FoodStatus{
   uneaten,
   eaten,
-  container
+  container,
+  view
 }
