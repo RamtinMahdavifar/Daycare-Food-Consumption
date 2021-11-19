6be2a76d3280b63493007b231127558dcf337aef
Shantanu Mishra <shantanu.mishra@usask.ca>
2021-11-18 20:38:35 -0600

    Added new Id and export to pdf button

--- lib/View/roster_page_widget.dart
+++ lib/View/roster_page_widget.dart
@@ -1,3 +1,4 @@
+import 'package:plate_waste_recorder/Helper/icons.dart';
 import 'package:responsive_flutter/responsive_flutter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -41,9 +42,9 @@ Widget RosterRecord(BuildContext context, String btnName, Widget Function() page
 
               ),
 
-            ),SizedBox(width: 100),
+            ),SizedBox(width: 200),
             Text(StudentID, style: TextStyle(fontSize: 40)),
-            SizedBox(width: 100),
+            SizedBox(width: 200),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(context, MaterialPageRoute(
@@ -64,7 +65,7 @@ Widget RosterRecord(BuildContext context, String btnName, Widget Function() page
               ),
 
             ),
-            SizedBox(width: 100),
+            SizedBox(width: 180),
             ElevatedButton(
           onPressed: () {
             Navigator.push(context, MaterialPageRoute(
@@ -84,27 +85,8 @@ Widget RosterRecord(BuildContext context, String btnName, Widget Function() page
 
   ),
 
-        ),SizedBox(width: 100),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                    builder: (context){
-                      return page();
-                    }));
+        )
 
-              },
-              style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                  textStyle:
-                  const TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-              child: Icon(
-                Icons.error,
-                color: Colors.black,
-                size: 60.0,
-
-              ),
-
-            ),
 
             ]
 
@@ -112,3 +94,80 @@ Widget RosterRecord(BuildContext context, String btnName, Widget Function() page
   );
 
 }
+
+Widget addNewId(BuildContext context, String btnName, Widget Function() page){
+  return Flexible(
+
+      child: SizedBox(
+          height: 100,
+          width: 650,
+
+          child: ElevatedButton(
+
+
+              onPressed: () {  Navigator.push(context, MaterialPageRoute(
+                  builder: (context){
+                    return page();
+                  })); },
+              child:Row(
+                  children:  <Widget>[
+
+                    Text(btnName,
+                      style: TextStyle(
+                        fontSize: ResponsiveFlutter.of(context).fontSize(4),// insert your font size here
+                      ),),
+
+                    Icon(
+                      Icons.person_add,
+                      color: Colors.orange,
+                      size: 50.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    )
+
+                  ]
+
+              )
+          )
+
+      )
+  );
+}
+
+Widget exportToPdf(BuildContext context, String btnName, Widget Function() page){
+  return Flexible(
+
+      child: SizedBox(
+          height: 100,
+          width: 650,
+
+          child: ElevatedButton(
+
+
+              onPressed: () {  Navigator.push(context, MaterialPageRoute(
+                  builder: (context){
+                    return page();
+                  })); },
+              child:Row(
+                  children:  <Widget>[
+
+                    Text(btnName,
+                      style: TextStyle(
+                        fontSize: ResponsiveFlutter.of(context).fontSize(4),// insert your font size here
+                      ),),
+
+                    Icon(
+                      Icons.picture_as_pdf,
+                      color: Colors.orange,
+                      size: 50.0,
+                      semanticLabel: 'Text to announce in accessibility modes',
+                    )
+
+                  ]
+
+              )
+          )
+
+      )
+  );
+}
+
