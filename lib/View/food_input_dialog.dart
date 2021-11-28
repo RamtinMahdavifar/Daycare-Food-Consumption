9a17309d3c48dbb9534d99a862d1e86ce03d02d8
Lec337 <lec337@mail.usask.ca>
2021-11-27 21:57:02 -0600

    food input dialog reads previously inputted meals from the database to be modified

--- lib/View/food_input_dialog.dart
+++ lib/View/food_input_dialog.dart
@@ -1,4 +1,6 @@
 
+import 'dart:convert';
+import 'package:plate_waste_recorder/Model/meal_info.dart';
 import 'package:flutter/services.dart';
 import 'package:image/image.dart' as image; // import this package with the name image to avoid naming collisions
 import 'package:flutter_native_screenshot/flutter_native_screenshot.dart';
@@ -16,6 +18,7 @@ import "../Model/variables.dart";
 import 'package:plate_waste_recorder/Helper/config.dart';
 import 'dart:io';
 import 'package:plate_waste_recorder/Model/database.dart';
+import 'package:firebase_database/firebase_database.dart'; // need to include for the Event data type
 
 
 class FoodInputDialog extends StatefulWidget {
@@ -34,8 +37,11 @@ class FoodInputDialog extends StatefulWidget {
 class _FoodInputDialogState extends State<FoodInputDialog> {
   List<String> exampleFoodItems = ["Apple", "Sandwich", "Juice"]; //for String foodItem in exampleFoodItems
   final nameTextController = new TextEditingController();
-  final weightTextController = new TextEditingController(text: "69"); //this value of text would be the value returned by our scale
+  final weightTextController = new TextEditingController();
   final commentsTextController = new TextEditingController();
+  // store the currently selected existing food item the user wants to modify or enter
+  // new data for if the user isn't entering in an entirely new food item
+  MealInfo selectedExistingFoodItem = null as MealInfo;
 
   List<String> FoodItems1 = ["Apple", "Sandwich", "Juice"]; //for String foodItem in existingFoodItems for eaten status
   List<String> Selected1 = []; //items that get selected once are not suggested again for eaten status
@@ -72,7 +78,7 @@ class _FoodInputDialogState extends State<FoodInputDialog> {
                             // new meal names to be input, otherwise the user must select between previously entered
                             // meals if they are entering a container or an eaten entry for an already served food
                             newFoodSubmission ? Column(children: [suggestBox(context, nameTextController, this._foodNameValid), itemPresets(nameTextController)])
-                                : SizedBox(height: 30, child: existingFoods(nameTextController)),
+                                : SizedBox(height: 30, child: existingFoods()),
                             // likewise only display previously created preset items if we are entering a food item for
                             // the first time
                             foodWeightField(weightTextController, "Weight(g)", this._foodWeightValid),
@@ -89,30 +95,106 @@ class _FoodInputDialogState extends State<FoodInputDialog> {
   }
 
 
-  Widget existingFoods(TextEditingController controller) {
-    List<Widget> presets = [];
-    if (widget.currentFoodStatus == FoodStatus.eaten){ // show remaining food items for eaten status
-      for (String foodItem in FoodItems1){
-        presets.add(
-            ElevatedButton(
-                onPressed: () {controller.text = foodItem;} ,
-                child: Text(foodItem)
-            )
-        );
-      }
-    }else if (widget.currentFoodStatus == FoodStatus.container){ //show remaining food items for container status
-      for (String foodItem in FoodItems2){
-        presets.add(
-            ElevatedButton(
-                onPressed: () {controller.text = foodItem;} ,
-                child: Text(foodItem)
-            )
-        );
-      }
-    }else {
-      throw Exception("Invalid Food Status");
+  Widget existingFoods() {
+    // we can only display existing meal/food items to add new data to if we are in
+    // either the container or eaten food state, otherwise we are viewing data or are
+    // submitting new uneaten food items instead of adding data to make an uneaten food
+    // item eaten or recording data for the container of an eaten food item
+    assert(widget.currentFoodStatus == FoodStatus.eaten || widget.currentFoodStatus == FoodStatus.container);
+    // determine the previous food status, as we can only enter eaten food values for a
+    // meal which has already been submitted as uneaten, likewise we can only enter container
+    // data for meals which have been submitted as eaten, so valid meals to add eaten data
+    // to are uneaten meals and valid meals to add container data to are eaten meals
+    FoodStatus previousFoodStatus;
+    if(widget.currentFoodStatus == FoodStatus.eaten){
+      previousFoodStatus = FoodStatus.uneaten;
     }
-    return ListView(scrollDirection: Axis.horizontal, children: presets);
+    else{
+      // current food status must be the container status
+      previousFoodStatus = FoodStatus.eaten;
+    }
+
+    return StreamBuilder<Event>(
+      stream: Database().readSubjectMealInfosWithStatus(ResearchGroupInfo("testResearchGroupName"), widget.currentInstitution, widget.currentSubject, previousFoodStatus),
+      builder: (BuildContext context, AsyncSnapshot<Event> snapshot) {
+        if (snapshot.hasError) {
+          Config.log.e("errors occurred while reading meal infos with status ${widget.currentFoodStatus} from the database on food input page, error: " + snapshot.error.toString());
+          return Text("errors in database read occurred");
+        }
+        else {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              Config.log.w(
+                  "connection state none when reading meal infos from the database");
+              // display a loading animation here, this will continue until we are
+              // able to connect to the database
+              return Center(child: CircularProgressIndicator(
+                value: null,
+                color: Colors.green,
+              ));
+              break;
+            case ConnectionState.waiting:
+              Config.log.i(
+                  "connection state waiting when reading meal infos from the database");
+              // include a waiting animation while we connect to the database
+              // value: null here indicates that the progress animation will
+              // continue forever, until we read data from the database
+              return Center(child: CircularProgressIndicator(
+                value: null,
+                color: Colors.green,
+              ));
+              break;
+            case ConnectionState.active:
+              Config.log.i(
+                  "active connection state when reading meal infos from the database");
+              DataSnapshot mealInfosSnapshot = snapshot.data!.snapshot;
+              if (mealInfosSnapshot.value == null) {
+                // if the retrieved mealInfosSnapshot contains null ie
+                // doesn't have any data, there are no meal infos
+                // present on the database for the current subject
+                // display a message indicating this
+                Config.log.w(
+                    "DataSnapshot has null value when reading meal infos from the database, ie no meal infos are present for the current subject");
+                return Center(child:
+                Text("No Food Items Have Yet Been Submitted",
+                    style: TextStyle(fontSize: 28.0))
+                );
+              }
+              else {
+                // otherwise we do have meal infos for the current subject on the database, display these
+                Config.log.i("Meal Infos present in DataSnapshot, displaying these");
+                Map<dynamic, dynamic> snapshotValueMap = mealInfosSnapshot
+                    .value as Map<dynamic, dynamic>;
+                String encodedMap = jsonEncode(snapshotValueMap);
+                Map<String, dynamic> mealInfoMap = json.decode(
+                    encodedMap) as Map<String, dynamic>;
+                // each key in this meal map is the ID of a meal, the corresponding
+                // value is the MealInfo object representing that meal, display each
+                // such MealInfo as a button in a listview
+                List<Widget> mealInfoList = mealInfoMap.values.map((element){
+                  // first convert each element in our values list to a MealInfo object
+                  MealInfo currentMealInfo = MealInfo.fromJSON(element);
+                  // display each meal info as a button, when the user clicks this button
+                  // store data for the selected meal info to be used later when data is
+                  // submitted
+                  return ElevatedButton(
+                      onPressed: () {
+                        Config.log.i("user has selected meal with name: ${currentMealInfo.name} and ID: ${currentMealInfo.mealId} to input new data for");
+                        selectedExistingFoodItem = currentMealInfo;
+                        },
+                      child: Text(currentMealInfo.name)
+                  );
+                }).toList();
+                return ListView(scrollDirection: Axis.horizontal, children: mealInfoList);
+              }
+
+            case ConnectionState.done:
+              Config.log.i("connection state done when reading institutions from the database");
+              return Text("connection state done");
+              break;
+          }
+        }
+      });
   }
 
   /// perform the image capture by screenshotting the whole screen, and then
