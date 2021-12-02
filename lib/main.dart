23210548b9a25a660da468b52114090c2989f6fe
Shantanu Mishra <shantanu.mishra@usask.ca>
2021-12-01 22:36:23 -0600

    Updated the main file to launch login page instead of Roster page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:plate_waste_recorder/Helper/config.dart';
-import 'View/roster_page.dart';
+import 'package:plate_waste_recorder/View/login_page.dart';
 class PlateWasteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -22,7 +22,7 @@ class PlateWasteApp extends StatelessWidget {
             caption: TextStyle(fontSize: 18.0),
           ),
         ),
-        home: Roster(),
+        home: LoginPage(),
     );
   }
 }
