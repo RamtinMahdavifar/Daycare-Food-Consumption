563fd03fe51cb83e800d3bb211e7223834a6e235
Lec337 <lec337@mail.usask.ca>
2021-12-03 03:31:48 -0600

    Removed unused methods, updated unit tests

--- test/unit_tests/meal_info_test.dart
+++ test/unit_tests/meal_info_test.dart
@@ -1,3 +1,4 @@
+import 'package:plate_waste_recorder/Model/food_status.dart';
 import 'package:plate_waste_recorder/Model/meal.dart';
 import 'package:plate_waste_recorder/Model/meal_info.dart';
 import 'package:test/test.dart';
@@ -5,26 +6,30 @@ import 'package:test/test.dart';
 void main(){
   group("meal info getters and setters",(){
     test("getters", (){
-      final MealInfo testMealInfo = MealInfo("test ID", "test name");
+      final MealInfo testMealInfo = MealInfo("test ID", "test name", FoodStatus.uneaten);
       expect(testMealInfo.mealId, "test ID");
+      expect(testMealInfo.mealStatus, FoodStatus.uneaten);
+      expect(testMealInfo.name, "test name");
     });
   });
 
   group("meal info operators",(){
     test("== operator",(){
-      final MealInfo testMealInfo = MealInfo("test ID", "test name");
+      final MealInfo testMealInfo = MealInfo("test ID", "test name", FoodStatus.uneaten);
 
       // test equality vs other objects
       expect(testMealInfo == "test", false);
       expect(testMealInfo == "", false);
       expect(testMealInfo == null, false);
-      expect(testMealInfo == Meal("test ID"), false);
+      expect(testMealInfo == "test value", false);
 
       // test equality vs MealInfo objects
-      expect(testMealInfo == MealInfo("something", "name"), false);
-      expect(testMealInfo == MealInfo("other ID", "test name"), false);
-      expect(testMealInfo == MealInfo("test ID", "other name"), false);
-      expect(testMealInfo == MealInfo("test ID", "test name"), true);
+      expect(testMealInfo == MealInfo("something", "name", FoodStatus.uneaten), false);
+      expect(testMealInfo == MealInfo("other ID", "test name", FoodStatus.uneaten), false);
+      expect(testMealInfo == MealInfo("test ID", "other name", FoodStatus.uneaten), false);
+      expect(testMealInfo == MealInfo("test ID", "test name", FoodStatus.eaten), false);
+      expect(testMealInfo == MealInfo("test ID", "test name", FoodStatus.container), false);
+      expect(testMealInfo == MealInfo("test ID", "test name", FoodStatus.uneaten), true);
       MealInfo otherMealInfo = testMealInfo;
       expect(testMealInfo == otherMealInfo, true);
       expect(testMealInfo == testMealInfo, true);
@@ -33,13 +38,13 @@ void main(){
 
   group("json operations",(){
     test("to json",(){
-      final MealInfo testMealInfo = MealInfo("test ID", "test name");
-      expect(testMealInfo.toJson(), <String, dynamic>{"mealId":"test ID", "databaseKey":"test ID", "name":"test name"});
+      final MealInfo testMealInfo = MealInfo("test ID", "test name", FoodStatus.uneaten);
+      expect(testMealInfo.toJson(), <String, dynamic>{"_mealId":"test ID", "databaseKey":"test ID", "name":"test name", "_mealStatus": "FoodStatus.uneaten"});
     });
 
     test("from json",(){
-      final Map<String,dynamic> testJSON = {"mealId":"test ID", "databaseKey":"test ID", "name":"test name"};
-      expect(MealInfo.fromJSON(testJSON), MealInfo("test ID", "test name"));
+      final Map<String,dynamic> testJSON = {"_mealId":"test ID", "databaseKey":"test ID", "name":"test name", "_mealStatus": "FoodStatus.uneaten"};
+      expect(MealInfo.fromJSON(testJSON), MealInfo("test ID", "test name", FoodStatus.uneaten));
     });
   });
 }
