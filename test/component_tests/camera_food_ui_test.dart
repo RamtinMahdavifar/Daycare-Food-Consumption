3e1d09e8290e8f0841ed104a3f514634b2a86d9c
bow770 <bo.wu@usask.ca>
2021-12-04 18:09:32 -0600

    investigate_failing_tests: refactor the camera food ui test

--- test/component_tests/camera_food_ui_test.dart
+++ test/component_tests/camera_food_ui_test.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:mockito/mockito.dart';
 import 'package:plate_waste_recorder/Helper/config.dart';
 import 'package:plate_waste_recorder/View/food_capture.dart';
+import 'package:plate_waste_recorder/Model/institution_info.dart';
+import 'package:plate_waste_recorder/Model/subject_info.dart';
+import 'package:plate_waste_recorder/Model/food_status.dart';
 
+class InstitutionInfoMock extends Mock implements InstitutionInfo{
+
+  @override
+  String get institutionAddress{
+    return "hihihihi";
+  }
+}
+
+class SubjectInfoMock extends Mock implements SubjectInfo{
+
+  @override
+  String get subjectId{
+    return "hi";
+  }
+}
 
 void main() {
   group("Capture Food Tests",() {
 
+    final InstitutionInfoMock currentInstitutionMock = InstitutionInfoMock();
+    final SubjectInfoMock subjectInfoMock = SubjectInfoMock();
+
     testWidgets('capture food item page tests', (WidgetTester tester) async {
       await tester.pumpWidget(MaterialApp(
-        home: CameraFood2(),
+        home: FoodCapture(currentInstitutionMock, subjectInfoMock, FoodStatus.eaten ),
       ));
       Config.log.i("Component Testing - capture food item page tests");
 
-      final cameraFoodHeader = find.text("Camera Food");
+      final cameraFoodEatenHeader = find.text("Capture Eaten Food Item");
 
       Config.log.i("Scan Student ID page header is present");
-      expect(cameraFoodHeader, findsOneWidget);
+      expect(cameraFoodEatenHeader, findsOneWidget);
     });
   });
 }
